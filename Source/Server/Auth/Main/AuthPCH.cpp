9e01ab805d6f72ce195226bb1b2ed0e6a42ed3f0
shanegxxiao <shanegxxiao@gmail.com>
2015-06-16 22:16:20 +0800

    add auth project and rename DarkForest.sln as Server.sln

---
+++ Source/Server/Auth/Main/AuthPCH.cpp
@@ -0,0 +1,1 @@
+#include "AuthPCH.h"
---
+++ Source/Server/Auth/Main/AuthPCH.h
@@ -0,0 +1,17 @@
+#ifndef __AuthPCH_h__
+#define __AuthPCH_h__
+
+#include <stdio.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+
+#include <boost/assert.hpp>
+
+#include "Network/Network.h"
+#include "Runtime/LogService.h"
+#include "Runtime/DynCreate.h"
+EXTERN_RUNTIME_EXPORTED_MEMBER
+
+#endif
